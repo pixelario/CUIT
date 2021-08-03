a43cd1326e4172e0238e8f3c507e42ba343fcf98
Ing. Bernardo Gabriel Raskovsky <bernardo@pixelario.com.ar>
2021-08-02 22:55:03 -0300

    Agrega extension OnlyValid

---
+++ src/CUIT/Extensions/OnlyValid.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pixelario.CUIT.Extensions
+{
+    public static partial class EnumerableMethods
+    {
+        public static IEnumerable<CUIT> OnlyValid(this IEnumerable<CUIT> input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException();
+            }
+            foreach(var cuit in input)
+            {
+                if(cuit.IsValid())
+                {
+                    yield return cuit;
+                }               
+            }
+        }
+    }
+}
