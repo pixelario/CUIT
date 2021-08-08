c3641e0ab3ff985aedbc0b4d24800769f6a28620
Ing. Bernardo Gabriel Raskovsky <bernardo@pixelario.com.ar>
2021-08-08 20:46:19 -0300

    Agrega la extensión RangeDocumento

---
+++ test/CUIT.UniTests/RangeDocumento.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+using Pixelario.CUIT.Extensions;
+
+namespace Pixelario.CUIT.UniTests
+{
+    public class RangeDocumento
+    {        
+        [Fact]
+        public void GetBetween27001001And27001999()
+        {
+            var input = new List<CUIT>() { 
+                new CUIT("20270010017"),
+                new CUIT("27230010012"),
+                new CUIT("23270019990"),
+                new CUIT("23270020004")
+            };
+            Assert.Equal(2, input.RangeDocumento(27001001, 998).Count());
+        }
+
+    }
+}
