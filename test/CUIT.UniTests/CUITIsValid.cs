b8597b898aa209dccc069a3e0c7909e4e3270f27
Ing. Bernardo Gabriel Raskovsky <bernardo@pixelario.com.ar>
2021-10-02 22:45:25 -0300

    Limpia de comentarios algunos tests

--- test/CUIT.UniTests/CUITIsValid.cs
+++ test/CUIT.UniTests/CUITIsValid.cs
@@ -7,18 +7,10 @@ namespace Pixelario.CUIT.UniTests
         public static TheoryData<TestCUITIsValid> CUITsTheoryData => new TheoryData<TestCUITIsValid>()
         {
             TestCUITIsValid.ValidCUITConParametros,
-            // TestCUITIsValid.InValidCUITConParametros,
             TestCUITIsValid.ValidCUITConString,
-            //TestCUITIsValid.InValidCUITConString,
             TestCUITIsValid.ValidCUITCortoConParametros,
-            //TestCUITIsValid.InValidCUITCortoConParametros,
             TestCUITIsValid.ValidCUITCortoConString,
-            //TestCUITIsValid.InValidCUITCortoConString,
             TestCUITIsValid.ValidCUITConLong,
-            //TestCUITIsValid.InValidCUITConMinLong,
-            //TestCUITIsValid.InValidCUITConMaxLong,
-            //TestCUITIsValid.ValidCUITCortoConLong,
-            //TestCUITIsValid.InValidCUITCortoConLong
         };
         
         [Theory]
--- test/CUIT.UniTests/TestCUITIsValid.cs
+++ test/CUIT.UniTests/TestCUITIsValid.cs
@@ -9,40 +9,19 @@
             tipoDeCUIT: TipoDeCUIT._20,
             numeroDeDocumento: 27001001,
             verificador: 7);
-        //public static readonly TestCUITIsValid InValidCUITConParametros = new TestCUITIsValid(
-        //    resultadoCorrecto: false,
-        //    tipoDeCUIT: TipoDeCUIT._23,
-        //    numeroDeDocumento: 27001001,
-        //    verificador: 7);
         public static readonly TestCUITIsValid ValidCUITConString = new TestCUITIsValid(
             resultadoCorrecto: true, "20270010017");
-        //public static readonly TestCUITIsValid InValidCUITConString = new TestCUITIsValid(
-        //    resultadoCorrecto: false, "23270010017");
         public static readonly TestCUITIsValid ValidCUITCortoConParametros = new TestCUITIsValid(
            resultadoCorrecto: true,
            tipoDeCUIT: TipoDeCUIT._20,
            numeroDeDocumento: 7001001,
            verificador: 2);
-        //public static readonly TestCUITIsValid InValidCUITCortoConParametros = new TestCUITIsValid(
-        //    resultadoCorrecto: false,
-        //    tipoDeCUIT: TipoDeCUIT._23,
-        //    numeroDeDocumento: 7001001,
-        //    verificador: 2);
         public static readonly TestCUITIsValid ValidCUITCortoConString = new TestCUITIsValid(
             resultadoCorrecto: true, "2070010012");
-        //public static readonly TestCUITIsValid InValidCUITCortoConString = new TestCUITIsValid(
-        //    resultadoCorrecto: false, "2370010012");
-
         public static readonly TestCUITIsValid ValidCUITConLong = new TestCUITIsValid(
             resultadoCorrecto: true, 20270010017);
         public static readonly TestCUITIsValid ValidCUITCortoConLong = new TestCUITIsValid(
            resultadoCorrecto: true, 20070010012);
-        //public static readonly TestCUITIsValid InValidCUITCortoConLong = new TestCUITIsValid(
-        //   resultadoCorrecto: false, 2070010012);
-        //public static readonly TestCUITIsValid InValidCUITConMinLong = new TestCUITIsValid(
-        //    resultadoCorrecto: false, 0);
-        //public static readonly TestCUITIsValid InValidCUITConMaxLong = new TestCUITIsValid(
-        //    resultadoCorrecto: false, 99999999999);
         protected TestCUITIsValid(bool resultadoCorrecto, string cuit)            
         {
             this.CUIT = CUIT.Parse(cuit: cuit);
