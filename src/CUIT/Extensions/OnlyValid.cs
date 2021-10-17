5efd12b6e3feab94b9d3d7fd06e25f6b5e56d95d
Bernardo Gabriel Raskovsky <bernardo@pixelario.com.ar>
2021-10-16 22:31:48 -0300

    Agrega comentarios a metodos publicos a las extensiones

--- src/CUIT/Extensions/OnlyValid.cs
+++ src/CUIT/Extensions/OnlyValid.cs
@@ -6,6 +6,10 @@ namespace Pixelario.CUIT.Extensions
 {
     public static partial class EnumerableMethods
     {
+        /// <summary>
+        /// Crea una IEnumerable<CUIT> con los <see cref="CUIT">CUIT</see> válidos de <paramref name="input"/>
+        /// </summary>
+        /// <returns>IEnumerable<CUIT> con <see cref="CUIT">CUIT</see>s válidos</returns>
         public static IEnumerable<CUIT> OnlyValid(this IEnumerable<CUIT> input)
         {
             if (input == null)
--- src/CUIT/Extensions/RangeDocumento.cs
+++ src/CUIT/Extensions/RangeDocumento.cs
@@ -5,6 +5,13 @@ namespace Pixelario.CUIT.Extensions
 {
     public static partial class EnumerableMethods
     {
+        /// <summary>
+        /// Crea una IEnumerable<CUIT> con un total de <paramref name="count"/> <see cref="CUIT">CUIT</see>s de <paramref name="input"/>
+        /// iniciando desde <paramref name="start"/>
+        /// </summary>
+        /// <param name="start">Valor del CUIT inicial</param>
+        /// <param name="count">Cantidad de <see cref="CUIT">CUIT</see>s</param>
+        /// <returns>IEnumerable<CUIT> con <see cref="CUIT">CUIT</see>s</returns>
         public static IEnumerable<CUIT> RangeDocumento(this IEnumerable<CUIT> input,
             int start, int count)
         {
